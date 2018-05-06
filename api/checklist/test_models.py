1dd046aa7a0029f60d82e83b8e020bc46991959d
Samuel <samuell.person@gmail.com>
2018-05-06 22:38:10 +0200

    refactor(api:checklist): rename test class

--- api/checklist/test_models.py
+++ api/checklist/test_models.py
@@ -1,8 +1,10 @@
 from utils.TestHelpers import DefaultModelTests
 from .models import Checklist
 
-class ModelTests(DefaultModelTests):
+class DefaultModelTests(DefaultModelTests):
     fixtures = [ 'checklist_fixture.json' ]
     test_objects = [
         { 'model': Checklist, 'string_rep': 'Checklist 1' } 
     ]
+
+
