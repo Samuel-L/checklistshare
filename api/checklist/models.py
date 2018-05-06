f1ac424cd45d7ad1baa9b02672cfb5014ec27007
Samuel <samuell.person@gmail.com>
2018-05-06 23:04:10 +0200

    feat(api:checklist:model): add UUIDField for url

--- api/checklist/models.py
+++ api/checklist/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import uuid
 
 class Checklist(models.Model):
     """
     Model for checklists
     """
     title = models.CharField(max_length=50)
+    url = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
 
     def __str__(self):
-        return self.title
+        return f'{self.title} - {self.url}'
