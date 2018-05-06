37a14449c9daceb6653b467d0d184e73413374da
Samuel <samuell.person@gmail.com>
2018-05-07 00:10:19 +0200

    feat(api:models): add Item model, fixture and test

--- api/checklist/models.py
+++ api/checklist/models.py
@@ -10,3 +10,13 @@ class Checklist(models.Model):
 
     def __str__(self):
         return f'{self.title} - {self.url}'
+
+class Item(models.Model):
+    """
+    Model for items
+    """
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.checklist.title} - {self.name}'
--- api/checklist/test_models.py
+++ api/checklist/test_models.py
@@ -1,10 +1,11 @@
 from utils.TestHelpers import DefaultModelTests
-from .models import Checklist
+from .models import Checklist, Item
 
 class DefaultModelTests(DefaultModelTests):
-    fixtures = [ 'checklist_fixture.json' ]
+    fixtures = [ 'checklist_fixture.json', 'item_fixture.json' ]
     test_objects = [
-        { 'model': Checklist, 'string_rep': 'Checklist 1 - 7f44d3f5-0155-48b5-8ffc-53d79dbcc0c2' } 
+        { 'model': Checklist, 'string_rep': 'Checklist 1 - 7f44d3f5-0155-48b5-8ffc-53d79dbcc0c2' },
+        { 'model': Item, 'string_rep': 'Checklist 1 - Item 1' }
     ]
 
 
