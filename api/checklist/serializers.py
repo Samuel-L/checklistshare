8474028e7302b6d71f52581d6c08a4bc1c921ed1
Samuel <samuell.person@gmail.com>
2018-05-07 00:28:35 +0200

    feat(api:serializers): add item serializer and tests

--- api/checklist/serializers.py
+++ api/checklist/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 
-from .models import Checklist
+from .models import Checklist, Item
 
 class ChecklistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Checklist
         fields = ('id', 'title', 'url')
+
+class ItemSerializer(serializers.ModelSerializer):
+    checklist = serializers.ReadOnlyField(source='checklist.title')
+
+    class Meta:
+        model = Item
+        fields = ('id', 'checklist', 'name')
--- api/checklist/test_serializers.py
+++ api/checklist/test_serializers.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
-from .serializers import ChecklistSerializer
-from .models import Checklist
+from .serializers import ChecklistSerializer, ItemSerializer
+from .models import Checklist, Item
 
 class ChecklistSerializerTests(TestCase):
     fixtures = [ 'checklist_fixture.json' ]
@@ -19,3 +19,20 @@ class ChecklistSerializerTests(TestCase):
     def test_serializer_fields_content(self):
         self.assertEqual(self.data['title'], self.checklist.title)
         self.assertEqual(self.data['url'], str(self.checklist.url))
+
+class ItemSerializerTests(TestCase):
+    fixtures = [ 'checklist_fixture.json', 'item_fixture.json' ]
+
+    def setUp(self):
+        self.item = Item.objects.get(pk=1)
+        self.serializer = ItemSerializer(instance=self.item)
+        self.data = self.serializer.data
+
+    def test_contains_expected_fields(self):
+        self.assertEqual(self.data.keys(), set([
+            'id', 'checklist', 'name'
+        ]))
+
+    def test_serializer_fields_content(self):
+        self.assertEqual(self.data['checklist'], self.item.checklist.title)
+        self.assertEqual(self.data['name'], self.item.name)
