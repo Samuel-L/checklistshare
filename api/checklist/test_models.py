4ed1af3e782d0a9a99ce1da779a793887021d3c5
Samuel <samuell.person@gmail.com>
2018-05-06 23:10:24 +0200

    fix(api:tests:models): fix checklist model test

--- api/checklist/test_models.py
+++ api/checklist/test_models.py
@@ -4,7 +4,7 @@ from .models import Checklist
 class DefaultModelTests(DefaultModelTests):
     fixtures = [ 'checklist_fixture.json' ]
     test_objects = [
-        { 'model': Checklist, 'string_rep': 'Checklist 1' } 
+        { 'model': Checklist, 'string_rep': 'Checklist 1 - 7f44d3f5-0155-48b5-8ffc-53d79dbcc0c2' } 
     ]
 
 
