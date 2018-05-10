95ff1611367fa96e6e9eef7c5351cfd3a39d22ae
Samuel <samuell.person@gmail.com>
2018-05-10 20:53:28 +0200

    feat(client:pages:Home): add CreateChecklistForm and test

---
+++ client/src/pages/Home/CreateChecklistForm.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from 'material-ui/styles';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+
+import ChecklistItem from './ChecklistItem';
+
+const styles = theme => ({
+  textField: {
+    margin: '5px',
+  },
+  buttonGroup: {
+    [theme.breakpoints.up('md')]: {
+      width: '150px',
+      float: 'right',
+    },
+    [theme.breakpoints.down('sm')]: {
+      position: 'absolute',
+      bottom: '0',
+      width: '100%',
+    },
+  },
+  buttonDiv: {
+    width: '100%',
+  },
+  button: {
+    width: '100%',
+    margin: '1px',
+    color: 'white',
+    backgroundColor: theme.palette.primary.A700,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.A700,
+      color: 'white',
+    },
+  },
+});
+
+const CreateChecklistForm = props => (
+  <form
+    id="create-list-form"
+    onSubmit={props.handleSubmit}
+  >
+    <TextField
+      required
+      id="name"
+      label="Title"
+      value={props.title}
+      onChange={props.handleTextFieldChange}
+      helperText="Please give your checklist a title"
+      fullWidth
+      margin="normal"
+      className={props.classes.textField}
+    />
+    <ChecklistItem
+      item={{ id: 0, name: '' }}
+      idx={0}
+      handleItemTextChange={() => {}}
+      handleItemDelete={() => {}}
+    />
+    <div className={props.classes.buttonGroup}>
+      <div className={props.classes.buttonDiv}>
+        <Button
+          className={props.classes.button}
+          variant="flat"
+          color="primary"
+        >
+          Add Item
+        </Button>
+      </div>
+      <div className={props.classes.buttonDiv}>
+        <Button
+          className={props.classes.button}
+          variant="flat"
+          color="primary"
+        >
+          Create List
+        </Button>
+      </div>
+    </div>
+  </form>
+);
+
+CreateChecklistForm.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleTextFieldChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+CreateChecklistForm.defaultProps = {};
+
+export default withStyles(styles)(CreateChecklistForm);
