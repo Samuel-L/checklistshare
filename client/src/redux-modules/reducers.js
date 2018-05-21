198ebf056aa98c204b9a2d5dfa530c115019788d
Samuel <samuell.person@gmail.com>
2018-05-21 18:19:20 +0200

    feat(client:redux-modules): add checklist fetcher reducer to reducer.js

--- client/src/redux-modules/reducers.js
+++ client/src/redux-modules/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import checklistAdderReducer from './checklist-adder';
+import checklistFetcherReducer from './checklist-fetcher';
 
 export default combineReducers({
   checklistAdderReducer,
+  checklistFetcherReducer,
 });
