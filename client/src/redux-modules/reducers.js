b6e201dafa7a4c5a0103ee82fd4ce163705b43fe
Samuel <samuell.person@gmail.com>
2018-05-23 20:29:14 +0200

    feat(client:redux-modules): add checklist-updater to root reducer

--- client/src/redux-modules/reducers.js
+++ client/src/redux-modules/reducers.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import checklistAdderReducer from './checklist-adder';
 import checklistFetcherReducer from './checklist-fetcher';
+import checklistUpdaterRedcuer from './checklist-updater';
 
 export default combineReducers({
   checklistAdderReducer,
   checklistFetcherReducer,
+  checklistUpdaterRedcuer,
 });
