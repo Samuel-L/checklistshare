5c553b5bcef3037167d6cb17047f7638472b40e1
Samuel <samuell.person@gmail.com>
2018-05-23 18:09:08 +0200

    feat(client:redux-modules): add checklist-updater reducer and tests

---
+++ client/src/redux-modules/checklist-updater.js
@@ -0,0 +1,45 @@
+const UPDATE_CHECKLIST_REQUEST = 'checklist-updater/UPDATE_CHECKLIST_REQUEST';
+const UPDATE_CHECKLIST_SUCCESS = 'checklist-updater/UPDATE_CHECKLIST_SUCCESS';
+const UPDATE_CHECKLIST_FAILURE = 'checklist-updater/UPDATE_CHECKLIST_FAILURE';
+const RESET_STATE = 'checklist-updater/RESET_STATE';
+
+export const actions = {
+  UPDATE_CHECKLIST_REQUEST,
+  UPDATE_CHECKLIST_SUCCESS,
+  UPDATE_CHECKLIST_FAILURE,
+  RESET_STATE,
+};
+
+export const initialState = {
+  updating: false,
+  updated: false,
+  error: 0,
+};
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    case UPDATE_CHECKLIST_REQUEST: {
+      return {
+        ...state, updating: true,
+      };
+    }
+    case UPDATE_CHECKLIST_SUCCESS: {
+      return {
+        ...state, updating: false, updated: true,
+      };
+    }
+    case UPDATE_CHECKLIST_FAILURE: {
+      return {
+        ...state, updating: false, error: action.payload,
+      };
+    }
+    case RESET_STATE: {
+      return {
+        ...initialState,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
---
+++ client/tests/redux-modules/checklist-updater.test.js
@@ -0,0 +1,37 @@
+import reducer, { actions, initialState } from '../../src/redux-modules/checklist-updater';
+
+describe('redux-modules: checklist-updater', () => {
+  describe('reducer', () => {
+    it('returns initial state', () => {
+      expect(reducer()).toEqual(initialState); 
+    });
+
+    it('handles UPDATE_CHECKLIST_REQUEST', () => {
+      const action = { type: actions.UPDATE_CHECKLIST_REQUEST };
+      const correctState = { ...initialState, updating: true };
+
+      expect(reducer(undefined, action)).toEqual(correctState);
+    });
+
+    it('handles UPDATE_CHECKLIST_SUCCESS', () => {
+      const action = { type: actions.UPDATE_CHECKLIST_SUCCESS };
+      const correctState = { ...initialState, updating: false, updated: true };
+
+      expect(reducer(undefined, action)).toEqual(correctState);
+    });
+
+    it('handles UPDATE_CHECKLIST_FAILURE', () => {
+      const action = { type: actions.UPDATE_CHECKLIST_FAILURE, payload: 400 };
+      const correctState = { ...initialState, updating: false, error: 400 };
+
+      expect(reducer(undefined, action)).toEqual(correctState);
+    });
+
+    it('handles RESET_STATE', () => {
+      const action = { type: actions.RESET_STATE };
+      const correctState = { ...initialState };
+
+      expect(reducer(undefined, action)).toEqual(correctState);
+    });
+  });
+});
