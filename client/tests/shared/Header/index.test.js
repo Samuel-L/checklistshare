01930e2f16607606a102cdf8f84e4428c68135e8
Samuel <samuell.person@gmail.com>
2018-05-26 18:05:44 +0200

    feat(shared:Header): add home button and update test

--- client/tests/shared/Header/index.test.js
+++ client/tests/shared/Header/index.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { HashRouter } from 'react-router-dom';
 
 import Header from '../../../src/shared/Header';
 
 describe('shared: Header', () => {
-  const tree = renderer.create(<Header />).toJSON();
+  const classes = { root: '', flex: '' };
+  const tree = renderer.create(<HashRouter><Header classes={classes} /></HashRouter>).toJSON();
 
   it('should render', () => {
     expect(tree).toMatchSnapshot(); 
