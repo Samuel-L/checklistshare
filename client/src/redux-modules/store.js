6492c84dadec19d6031330fd1b485632ea82ce63
Samuel <samuell.person@gmail.com>
2018-05-28 23:04:10 +0200

    refactor(redux-modules): refactor store to be production ready

--- client/src/redux-modules/store.js
+++ client/src/redux-modules/store.js
@@ -1,12 +1,7 @@
-import { applyMiddleware, createStore, compose } from 'redux';
-import promise from 'redux-promise-middleware';
-import thunk from 'redux-thunk';
+/* eslint-disable global-require */
 
-import reducer from './reducers';
-
-const middleware = applyMiddleware(promise(), thunk);
-/* eslint-disable no-underscore-dangle */
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-/* eslint-enable */
-
-export default createStore(reducer, composeEnhancers(middleware));
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./store.prod.js');
+} else {
+  module.exports = require('./store.dev.js');
+}
