a5b1deefddf0f033975cc7e4f0d1ef04760ab12c
Samuel <samuell.person@gmail.com>
2018-05-30 16:20:42 +0200

    refactor(clientUtils:axios-helpers): baseURL is different based on if NODE_ENV is prod or dev

--- client/src/utils/axios-helpers.js
+++ client/src/utils/axios-helpers.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const baseURL = 'https://checklistshareapi.herokuapp.com/api';
+let baseURL;
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://checklistshareapi.herokuapp.com/api';
+} else {
+  baseURL = 'localhost:8000/api';
+}
 
 export const axiosInstance = axios.create({
   baseURL,
